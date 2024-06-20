95c56641c9b4fce0962be86a2c03eb46d85261f6
Abdyushev Rafail <Abdyushev.R@romir.ru>
2024-06-20 12:26:52 +0300

    outlook

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import sys, os
+import asyncio
+# import aiohttp
 import dns.resolver
 import socket
 from pathlib import Path
@@ -7,8 +9,43 @@ from email_validator import validate_email, EmailNotValidError
 import yagmail
 from email.message import EmailMessage
 from dotenv import load_dotenv
+from email.mime.text import MIMEText
 
-def sendLyb(args):
+async def sendOutlook(args):
+    # smtp_server = "smtp-mail.outlook.com"
+    smtp_server = "smtp.lancloud.ru"
+    port = 587  # используйте порт 465 для SSL
+    server = smtplib.SMTP(smtp_server, port)
+    server.starttls() 
+    email = "abdyushev.r@romir.ru"
+    data = os.getenv('PASSWORD_OUTLOOK')
+    server.login(email, data)
+    from_email = email
+    to_email = "rafaelab@mail.ru"
+    
+    title = 'Добрый день'
+    msg_content = '<h2>{title}</h2>'.format(title=title)
+    msg_content += '<h4>Ваш код - 7893</h4>'
+    message = MIMEText(msg_content, 'html')
+    message['From'] = 'abdyushev.r@romir.ru'
+    message['To'] = 'rafaelab@mail.ru'
+    message['Subject'] = 'from outlook'
+    msg_full = message.as_string()
+    # message = "<h2>hello world</h2>"
+    # server.sendmail(from_email, to_email, f"Subject: {subject}\n\n{msg_full}")
+    try:
+        server.sendmail(from_email, to_email, msg_full)
+    except (smtplib.SMTPRecipientsRefused, smtplib.SMTPSenderRefused) as e:
+        print(e)
+    server.quit()
+
+    # subject = "from outlook"
+    # message = "hello world"
+    # res = server.sendmail(from_email, to_email, f"Subject: {subject}\n\n{message}")
+    # print(res)
+    # server.quit()
+
+async def sendLyb(args):
     smtp_server = "smtp.gmail.com"
     port = 587  # используйте порт 465 для SSL
     server = smtplib.SMTP(smtp_server, port)
@@ -18,41 +55,55 @@ def sendLyb(args):
     server.login(email, data)
     from_email = email
     to_email = "rafaelab@mail.ru"
-    subject = "test"
-    message = "test"
-    server.sendmail(from_email, to_email, f"Subject: {subject}\n\n{message}")
+    
+    # subject = "test"
+    title = 'Добрый день'
+    msg_content = '<h2>{title}</h2>'.format(title=title)
+    msg_content += '<h4>Ваш код - 7893</h4>'
+    message = MIMEText(msg_content, 'html')
+    message['From'] = 'rafradio@gmail.com'
+    message['To'] = 'rafaelab@mail.ru'
+    message['Subject'] = 'Any subject'
+    msg_full = message.as_string()
+    # message = "<h2>hello world</h2>"
+    # server.sendmail(from_email, to_email, f"Subject: {subject}\n\n{msg_full}")
+    try:
+        server.sendmail(from_email, to_email, msg_full)
+    except (smtplib.SMTPRecipientsRefused, smtplib.SMTPSenderRefused) as e:
+        print(e)
     server.quit()
 
     
         
 
-def main(args):
+def optionMain(args):
     records = []
     my_resolver = dns.resolver.Resolver()
     # answers = my_resolver.resolve(host, "A")
-    answer_txt = my_resolver.resolve('gmail.com', "MX")
+    # answer_txt = my_resolver.resolve('megamanager24.ru', "MX")
+    answer_txt = my_resolver.resolve('abduyshev.ru', "MX")
     records.extend([str(answer) for answer in answer_txt])
     print(records)
     mxRecord = records[0]
     mxRecord = str(mxRecord)
     host = socket.gethostname()
-    print(host)
-    smtp_server = "smtp.gmail.com"
-    context = ssl.create_default_context()
-    server = smtplib.SMTP()
-    server.set_debuglevel(0)
-    server.connect(smtp_server)
-    server.ehlo() 
-    server.starttls(context=context)
-    server.helo(host)
-    server.mail('rafaelab@mail.ru')
-    code, message = server.rcpt(str("rafradio@gmail.com"))
-    server.quit()
+    # print(host)
+    # smtp_server = "smtp.gmail.com"
+    # context = ssl.create_default_context()
+    # server = smtplib.SMTP()
+    # server.set_debuglevel(0)
+    # server.connect(smtp_server)
+    # server.ehlo() 
+    # server.starttls(context=context)
+    # server.helo(host)
+    # server.mail('rafaelab@mail.ru')
+    # code, message = server.rcpt(str("rafradio@gmail.com"))
+    # server.quit()
     
-    if code == 250:
-        print('Success')
-    else:
-        print('Bad', message)
+    # if code == 250:
+    #     print('Success')
+    # else:
+    #     print('Bad', message)
 
     # server.mail('rafaelab@mail.ru')
     # code, message = server.rcpt(str('rafaelab@mail.ru'))
@@ -61,8 +112,21 @@ def main(args):
     #   print('Success')
     # else:
     #     print('Bad')
+    
+async def srartAsync(args):
+    tasks = []
+    # task = asyncio.ensure_future(sendLyb(args))
+    task = asyncio.ensure_future(sendOutlook(args))
+    tasks.append(task)
+    await asyncio.gather(*tasks)
 
 
 if __name__ == "__main__":
     load_dotenv()
-    sendLyb(sys.argv)
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    # loop = asyncio.get_event_loop()
+    loop.run_until_complete(srartAsync(sys.argv))
+    print('hello world')
+    # sendOutlook(sys.argv)
+    # optionMain(sys.argv)
